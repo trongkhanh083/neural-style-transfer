4da871d4be5533ea1f78627481502dd771c40a1c
trongkhanh083 <trongkhanh083@gmail.com>
2025-10-23 16:40:37 +0700

    update data train 2

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ mkdir results
 mkdir data
 cd data
 wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
-wget http://images.cocodataset.org/zips/test2015.zip
-unzip -q test2015.zip
-rm -rf test2015.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+unzip -q val2014.zip
+rm -rf val2014.zip
