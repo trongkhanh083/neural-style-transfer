b0630fa22a6520ff349875a4aa898e87a5d0f6c6
trongkhanh083 <trongkhanh083@gmail.com>
2025-10-23 14:47:29 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+mkdir checkpoints
+
 mkdir data
 cd data
 wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 wget http://images.cocodataset.org/zips/val2017.zip
 unzip -q val2017.zip
+rm -rf val2017.zip
