4e9747e17cc1573f623eeb27f6751af2ec68b52e
trongkhanh083 <trongkhanh083@gmail.com>
2025-10-23 14:28:31 +0700

    initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir data
+cd data
+wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+wget http://images.cocodataset.org/zips/val2017.zip
+unzip -q val2017.zip
